8793d7b782cd2f075dba896e00de519ea1f4b7a9
LuYiFang <technology99x@gmail.com>
2014-03-09 13:01:16 +0800

    add formula
    can not use sin(x)

--- src/abstractclass/Abstractclass.java
+++ src/abstractclass/Abstractclass.java
@@ -1,33 +1,114 @@
 package abstractclass;
-
+import java.awt.*;
+import javax.swing.*;
 public class Abstractclass {
+	public static void main(String[] args){
+		int x = 0;
+		
+		JFrame frame = new JFrame();
+        frame.add(new JPanel()); 
+        frame.setSize(1000,400);
+        frame.setVisible(true);
+        frame.getContentPane().add(new TestFunctionA());
+        frame.getContentPane().add(new TestFunctionB());
+        frame.getContentPane().add(new TestFunctionC());
+        frame.getContentPane().setLayout(new GridLayout(1, 3,6,7));
+		JPanel panel = new JPanel();
+		panel.setVisible(true);
+	}
 
 }
 
-public abstract class AbstractDrawFunction extends JPanel{
+abstract class AbstractDrawFunction extends JPanel{
+	
+	private static final long serialVersionUID = 1L;
 	private Polygon p = new Polygon();
+	
 	protected AbstractDrawFunction(){
-		DrawFunction();
+		drawFunction();
 	}
 	
 	abstract double f(double x);
 	
 	public void drawFunction(){
 		for(int x = -100; x <= 100;x++){
-			p.addpoint(x + 200,200 - (int)f(x))
+			p.addPoint(x + 200,200 - (int)f(x));
 		}
 	}
 	
 	@Override
-	protected void paintCompoent(Graphics g){
+	protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+		drawFunction();
+		g.drawLine(10, 200,300 ,200);
+		g.drawLine(310,200, 410, 200);
+		g.drawLine(420, 200, 520, 200);
+		g.drawLine(200, 10,200 ,200);
+		g.drawLine(200, 210,200 ,310);
+		g.drawLine(500, 200,500 ,100);
+		g.drawPolyline(p.xpoints,p.ypoints,p.npoints);
 	}
 }
 
 class TestFunctionA extends AbstractDrawFunction{
 	
-	public functionA(){
-		
-		
+	private static final long serialVersionUID = 1L;
+
+	public double f(double x){
+		return Math.pow(x, 2);
 	}
+}
+
+class TestFunctionB extends AbstractDrawFunction{
 	
+	private static final long serialVersionUID = 1L;
+	
+	public double f(double x){
+		return 50 * Math.sin(x / 100 * 2 * Math.PI);
+	}
+}
+
+class TestFunctionC extends AbstractDrawFunction{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public double f(double x){
+		return 50 * Math.cos(x / 100 * 2 * Math.PI);
+	}
+}
+
+class TestFunctionD extends AbstractDrawFunction{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public double f(double x){
+		return Math.tan(x);
+	}
+}
+
+class TestFunctionE extends AbstractDrawFunction{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public double f(double x){
+		return Math.cos(x) + 5 * Math.sin(x);
+	}
+}
+
+class TestFunctionF extends AbstractDrawFunction{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public double f(double x){
+		return 5 * Math.cos(x) + Math.sin(x);
+	}
+}
+
+class TestFunctionG extends AbstractDrawFunction{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public double f(double x){
+		return Math.log(x);
+	}
 }
